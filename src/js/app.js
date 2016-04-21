0e1f4f487c227e9a84e8e852b808d4c107951823
zw-workspace <814385839@qq.com>
2016-04-21 09:15:35 +0800

    Initial commit

---
+++ src/js/app.js
@@ -0,0 +1,40 @@
+/**
+ * Created by dell on 2016/4/18.
+ */
+angular.module('myApp',
+    [
+        'ngRoute',
+        'appController'
+    ])
+    .config(
+        ['$routeProvider',function($routeProvider){
+            $routeProvider.
+                when('/', {
+                    controller: 'LoginController',
+                    templateUrl: 'modules/login/login.html'
+                }).
+                when('/modules',{
+                    controller:'ModulesController',
+                    templateUrl:'modules/modules.html'
+                }).
+                when('/modules/test1', {
+                    controller: 'Test1Controller',
+                    templateUrl: 'modules/test1/test1.html'
+                }).
+                when('/modules/test2', {
+                    controller: 'Test2Controller',
+                    templateUrl: 'modules/test2/test2.html'
+                }).
+                when('/modules/test3', {
+                    controller: 'Test3Controller',
+                    templateUrl: 'modules/test3/test3.html'
+                }).
+                when('/modules/test4', {
+                    controller: 'Test4Controller',
+                    templateUrl: 'modules/test4/test4.html'
+                }).
+                otherwise({
+                    redirectTo: '/'
+                });
+        }]
+    );
---
+++ src/js/controllers.js
@@ -0,0 +1,42 @@
+/**
+ * Created by dell on 2016/4/19.
+ */
+angular.module('appController',[])
+    .controller('LoginController',
+        ['$scope','$http',function($scope,$http,$location){
+
+            $scope.verific = function(){
+
+                $http.post("../php/hello.php",$scope.user).then(function (response) {
+                    console.log(response);
+                    if(response.data && response.data.new_id){
+                        $location.href = "modules/"+response.data.new_id;
+                        console.log(response.data);
+                    }else{
+                        $scope.validateTip = response.data.ERROR;
+                    }
+                });
+            }
+        }]
+    )
+    .controller('ModulesController',
+        ['scope',function($scope){
+
+        }])
+    .controller('Test1Controller',
+        ['$scope',function($scope){
+
+        }])
+    .controller('Test2Controller',
+        ['$scope',function($scope){
+
+        }])
+    .controller('Test3Controller',
+        ['$scope',function($scope){
+
+        }])
+    .controller('Test4Controller',
+        ['$scope',function($scope){
+
+        }]
+    );
